e80ad83f1a4eadd78c70e940b4d15e2d7c91e123
Siavash <119043855+Abyys001@users.noreply.github.com>
2023-07-22 17:10:38 +0330

    create model for profile app

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    username = models.CharField(max_length=16, unique=True)
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50, blank=True)
+    gmail = models.EmailField(max_length=254)
+    password = models.CharField(max_length=50)
+    created_time = models.DateTimeField(auto_now_add=True)
+    last_modify = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"{self.username}"
+    
+    
+
+
+
+
--- weblog/settings.py
+++ weblog/settings.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     # restAPI
     'rest_framework',
     # My Apps
-    'posts'
+    'posts',
+    'profiles',
+    
 ]
 
 MIDDLEWARE = [
