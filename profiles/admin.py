980992be91c6d1a60ef411a53974c5347c542548
Siavash <119043855+Abyys001@users.noreply.github.com>
2023-07-23 17:51:40 +0330

    update admin.py of profiles

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Profile
 # Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    model = Profile
+    list_display = ["username", "fname", "last_modify"]
--- weblog/settings.py
+++ weblog/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # restAPI
     'rest_framework',
-    # My Apps
+    # local
     'posts',
     'profiles',
     
