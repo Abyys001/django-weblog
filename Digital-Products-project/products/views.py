29485df24e78af601623a09b540ecae3d83976d1
Siavash <119043855+Abyys001@users.noreply.github.com>
2023-08-02 18:36:25 +0330

    Update views.py

--- Digital-Products-project/products/views.py
+++ Digital-Products-project/products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
 
-# Create your views here.
+from .models import Category, File, Product
+from .serializer import CategorySerializer, FileSerializer, ProductSerializer
+
+
+class ProductList(View):
+    
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return HttpResponse(serializer)
